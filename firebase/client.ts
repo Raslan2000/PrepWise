96da1432cb016722b7ff09cd700bdb5d2dbd7e19
Raslan Kamal Batcha <rockstarraslan2000@gmail.com>
2025-05-14 21:56:11 -0400

    Firebase Setup

---
+++ firebase/client.ts
@@ -0,0 +1,17 @@
+import {cert, getApp, getApps, initializeApp} from "firebase-admin/app";
+import {getAuth} from "firebase-admin/auth";
+import {getFirestore} from "firebase-admin/firestore";
+const firebaseConfig = {
+  apiKey: "AIzaSyDNHKZJ8TSkNqYnZzfJTHDJ_g0gyeCwrEs",
+  authDomain: "prepwise-b493f.firebaseapp.com",
+  projectId: "prepwise-b493f",
+  storageBucket: "prepwise-b493f.firebasestorage.app",
+  messagingSenderId: "674459501438",
+  appId: "1:674459501438:web:5aa7ba1829dd637842a92f",
+  measurementId: "G-2ZKSY10BW4"
+};
+
+// Initialize Firebase
+const app = !getApps.length? initializeApp(firebaseConfig): getApp()
+export const auth = getAuth(app)
+export const db = getFirestore(app)
