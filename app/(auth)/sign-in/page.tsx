2ac78d34946fae6e5b69ed531a91424bf460e727
Raslan Kamal Batcha <rockstarraslan2000@gmail.com>
2025-05-14 20:48:16 -0400

    Initial set up - Home Page and layout

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,8 @@
+import AuthForm from '@/components/AuthForm'
+import React from 'react'
+
+const layout = () => {
+  return <AuthForm type="sign-in"/>
+}
+
+export default layout
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,8 @@
+import AuthForm from '@/components/AuthForm'
+import React from 'react'
+
+const layout = () => {
+  return <AuthForm type="sign-up"/>
+}
+
+export default layout
